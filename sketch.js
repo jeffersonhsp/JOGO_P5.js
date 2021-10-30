0bc6b333aade2e858c1ab857bb02a1de0a3a010d
jeffersonhsp <38038782+jeffersonhsp@users.noreply.github.com>
2021-10-30 15:36:52 -0300

    Delete sketch.js

--- sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    createCanvas(500, 500);
-}
-
-function draw() {
-    background(mouseX/3,mouseY-50/3,(mouseX*mouseY-50)/255);
-    text("mouse X: "+mouseX,15,300);
-    text("mouse Y: "+mouseY,15,50);
-}
